d3f9a4b02bf6597531226f8159ce5cb185113d2f
Ved <vedp3705@gmail.com>
2025-02-20 14:19:09 -0800

    test commit for file check

--- train_yolo.py
+++ train_yolo.py
@@ -4,7 +4,7 @@ from dataset_loader import WaymoDataset, raw_dataset
 
 model = YOLO("yolov8n.pt") 
 
-dataset = WaymoDataset(raw_dataset)
+dataset = WaymoDataset(raw_dataset)s
 dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
 
 model.train(data="config.yaml", epochs=50, dataloader=dataloader)
